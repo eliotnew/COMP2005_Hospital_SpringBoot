1119b879444d410810595aab6d55f89a11beca10
Eliot New <eliot.new@students.plymouth.ac.uk>
2023-05-04 16:01:30 +0100

    Add all

---
+++ src/main/java/com/example/EliotCOMP2005CW2/EliotComp2005Cw2Application.java
@@ -0,0 +1,13 @@
+package com.example.EliotCOMP2005CW2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EliotComp2005Cw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EliotComp2005Cw2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EliotCOMP2005CW2/EliotComp2005Cw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EliotCOMP2005CW2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EliotComp2005Cw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
