45f13f324ab29960a69ea9e22ebdff8fb08a0c61
Eliot New <eliot.new@students.plymouth.ac.uk>
2023-05-05 17:43:24 +0100

    Just need to get three day discharge outputting correctly and im ready to test

--- src/main/java/com/example/EliotCOMP2005CW2/Controllers/Three_Day_Discharge_Controller.java
+++ src/main/java/com/example/EliotCOMP2005CW2/Controllers/Three_Day_Discharge_Controller.java
@@ -1,7 +1,9 @@
 package com.example.EliotCOMP2005CW2.Controllers;
 
 import com.example.EliotCOMP2005CW2.Admission;
+import com.example.EliotCOMP2005CW2.Allocation;
 import com.google.gson.Gson;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -22,15 +24,28 @@ import java.util.List;
 @RestController
 public class Three_Day_Discharge_Controller {
 
-    //I made this function in the first attempt of this project which was not springboot
-    //It returns a list of patient IDS where the stay was >= 3days
-    //
     @GetMapping("/3daydischarge")
     public List<Integer> getPatients3Days() {
-        String API_URL = "https://web.socem.plymouth.ac.uk/COMP2005/api/Admissions";
-        List<Integer> patientIDs = new ArrayList<>();
+
         try {
-            // HTTP GEt request
+
+            Admission[] admissions = getAdmissions();//Gets all admissions
+
+            List<Integer> patientsIDs = getShortStays(admissions); // Sorts through them retreiving IDs of patients with a short stay
+
+            return patientsIDs;
+
+        }catch(Exception e ){
+            System.out.println("Went wrong");
+            int size = 1;
+            List<Integer> failList = new ArrayList<>(Collections.nCopies(size, -1));
+
+            return failList;
+        }
+    }
+    public Admission[] getAdmissions(){
+        String API_URL = "https://web.socem.plymouth.ac.uk/COMP2005/api/Admissions";
+        try{
             CloseableHttpClient httpClient = HttpClientBuilder.create().build();
             HttpGet request = new HttpGet(API_URL);
             request.addHeader("accept", "text/plain");
@@ -40,31 +55,37 @@ public class Three_Day_Discharge_Controller {
             BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
             Gson gson = new Gson();
             Admission[] admissionArray = gson.fromJson(reader, Admission[].class);
-            List<Admission> admissionList = Arrays.asList(admissionArray);
 
-            //  Filter each admissions where the stay is less than or equal to 3 days
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
-            for (Admission admission : admissionList) {
+            return admissionArray;
 
-                //Gets the formatted dates per each admission entry and calculates the duration of stay in days
-
-                LocalDateTime admissionDate = LocalDateTime.parse(admission.getAdmissionDate(), formatter);
-                LocalDateTime dischargeDate = LocalDateTime.parse(admission.getDischargeDate(), formatter);
-                Duration duration = Duration.between(admissionDate, dischargeDate);
-
-                long daysStay = duration.toDays();
-                if (daysStay <= 3) {
-                    patientIDs.add(admission.getPatientID());
-                }
-            }
-
-            return patientIDs;
-        }catch(Exception e ){
-            System.out.println("Went wrong");
-            int size = 1;
-            List<Integer> failList = new ArrayList<>(Collections.nCopies(size, -1));
-
-            return failList;
+        }catch (Exception e){
+            System.out.println("Failed to get admissions.");
+            Admission[] failed = new Admission[0];
+            return failed;
         }
+
+    }
+    private List<Integer> getShortStays(Admission[] admissions){
+
+        List<Integer> patientIDs  = new ArrayList<>();
+        List<Admission> admissionList = Arrays.asList(admissions);
+
+        //  Filter each admissions where the stay is less than or equal to 3 days
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+        for (Admission admission : admissionList) {
+
+            //Gets the formatted dates per each admission entry and calculates the duration of stay in days
+
+            LocalDateTime admissionDate = LocalDateTime.parse(admission.getAdmissionDate(), formatter);
+            LocalDateTime dischargeDate = LocalDateTime.parse(admission.getDischargeDate(), formatter);
+            Duration duration = Duration.between(admissionDate, dischargeDate);
+
+            long daysStay = duration.toDays();
+
+            if (daysStay <= 3) {
+                patientIDs.add(admission.getPatientID());
+            }
+        }
+        return patientIDs;
     }
 }
