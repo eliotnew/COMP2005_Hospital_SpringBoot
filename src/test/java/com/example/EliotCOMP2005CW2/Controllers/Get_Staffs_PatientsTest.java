fb23058ce8656a384fa2c1ba633028352559d675
Eliot New <eliot.new@students.plymouth.ac.uk>
2023-05-07 15:58:23 +0100

    Finished the third unit test class

--- src/test/java/com/example/EliotCOMP2005CW2/Controllers/Get_Staffs_PatientsTest.java
+++ src/test/java/com/example/EliotCOMP2005CW2/Controllers/Get_Staffs_PatientsTest.java
@@ -4,6 +4,7 @@ import com.example.EliotCOMP2005CW2.Allocation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -280,6 +281,51 @@ class Get_Staffs_PatientsTest {
 
         assertEquals(1,testList.size());
         assertEquals(123,testList.get(0));
+    }
+
+    @Test
+    public void testRemoveDuplicates(){
+        List<Integer> testList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            testList.add(1);
+
+        }
+        List <Integer> removed = get_staffs_patients.RemoveDuplicates(testList);
+        assertEquals(1,removed.size());
+        assertEquals(1,removed.get(0));
+    }
+    @Test
+    public void testRemoveDuplicates_withNegatives(){
+        List<Integer> testList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            testList.add(-4);
+
+        }
+        List <Integer> removed = get_staffs_patients.RemoveDuplicates(testList);
+        assertEquals(1,removed.size());
+        assertEquals(-4,removed.get(0));
+    }
+    @Test
+    public void testRemoveDuplicates_massiveList(){
+        List<Integer> testList = new ArrayList<>();
+        for (int i = 0; i < 99999; i++) {
+            testList.add(1);
+
+        }
+        List <Integer> removed = get_staffs_patients.RemoveDuplicates(testList);
+        assertEquals(1,removed.size());
+        assertEquals(1,removed.get(0));
+    }
+    @Test
+    public void testRemoveDuplicates_noDupesMassive(){
+        List<Integer> testList = new ArrayList<>();
+        for (int i = 0; i < 99999; i++) {
+            testList.add(i);
+
+        }
+        List <Integer> removed = get_staffs_patients.RemoveDuplicates(testList);
+        assertEquals(99999,removed.size());
+        assertEquals(0,removed.get(0));
     }
 
 
