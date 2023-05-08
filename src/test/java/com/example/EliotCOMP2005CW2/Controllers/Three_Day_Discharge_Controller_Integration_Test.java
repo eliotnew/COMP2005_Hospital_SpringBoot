1b063b01054a4402381245f1e11327841849c2ee
Eliot New <eliot.new@students.plymouth.ac.uk>
2023-05-08 16:15:57 +0100

    nothing works with integration or functional testing due to the issue with the null typing

--- src/test/java/com/example/EliotCOMP2005CW2/Controllers/Three_Day_Discharge_Controller_Integration_Test.java
+++ src/test/java/com/example/EliotCOMP2005CW2/Controllers/Three_Day_Discharge_Controller_Integration_Test.java
@@ -1,26 +1,17 @@
 package com.example.EliotCOMP2005CW2.Controllers;
 
 import com.example.EliotCOMP2005CW2.Admission;
-import org.apache.http.HttpEntity;
-import org.apache.http.StatusLine;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 //I beleive it is a correct mock test but again lists are not being made properly
 @ExtendWith(MockitoExtension.class)
 public class Three_Day_Discharge_Controller_Integration_Test {
@@ -28,7 +19,7 @@ public class Three_Day_Discharge_Controller_Integration_Test {
 
     private Three_Day_Discharge_Controller mockThreeDay = mock(Three_Day_Discharge_Controller.class);
 
-    @Before
+    @BeforeEach
     public void setup(){
         Admission admissionTest = new Admission();
 
@@ -50,6 +41,7 @@ public class Three_Day_Discharge_Controller_Integration_Test {
         Integer resultGot = result.get(0);
 
         assertEquals(78,resultGot);
+        assertEquals(1,result.size());
     }
 
 
