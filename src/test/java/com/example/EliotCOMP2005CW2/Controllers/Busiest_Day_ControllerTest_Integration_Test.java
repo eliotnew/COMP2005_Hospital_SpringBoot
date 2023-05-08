b1303a2a645d1524856837172bd5be6a8675d4bf
Eliot New <eliot.new@students.plymouth.ac.uk>
2023-05-08 14:33:43 +0100

    Integration tests still not working

--- src/test/java/com/example/EliotCOMP2005CW2/Controllers/Busiest_Day_ControllerTest_Integration_Test.java
+++ src/test/java/com/example/EliotCOMP2005CW2/Controllers/Busiest_Day_ControllerTest_Integration_Test.java
@@ -2,6 +2,7 @@ package com.example.EliotCOMP2005CW2.Controllers;
 
 import com.example.EliotCOMP2005CW2.Admission;
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -14,7 +15,7 @@ import static org.mockito.Mockito.when;
 class Busiest_Day_ControllerTest_Integration_Test {
     private Busiest_Day_Controller mockBusiest_day_controller = mock(Busiest_Day_Controller.class);
 
-    @Before
+    @BeforeEach
     public void setup(){
         Admission admissionTest = new Admission();
 
@@ -37,7 +38,9 @@ class Busiest_Day_ControllerTest_Integration_Test {
     @Test
     public void assessString(){
         String busiestday = mockBusiest_day_controller.busiestDay(); //returns null apparently
+        System.out.println(busiestday);
         boolean isFriday = busiestday.contains("Friday");
+        System.out.println(isFriday);
         assertTrue(isFriday);
 
     }
